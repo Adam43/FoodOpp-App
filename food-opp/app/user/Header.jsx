7334827f82be8d93c3e4901f1b107db364f1ed62
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2023-05-19 15:29:13 -0400

    Added RequestFood component;

--- food-opp/app/user/Header.jsx
+++ food-opp/app/user/Header.jsx
@@ -1,27 +1,31 @@
-// `app/page.tsx` is the UI for the `/` URL
+import {} from "react";
+import Logo from "../../public/logo.svg"
+
+
+
 export default function Header() {
+
+  // const result = await 
+
+  const userName = "Bob"
+  
     return (
-      <header className="text-gray-600 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center text-gray-500 mb-4 md:mb-0">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
-            <span className="ml-3 text-xl">Tailblocks</span>
+      <header className="text-gray-600 flex flex-wrap self-center border border-red-300 w-full bg-purple-900 p-0!">
+        <div className="container mx-1 flex flex-wrap p-1 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-100 mb-4 md:mb-0">
           </a>
-          <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-            <a className="mr-5 hover:text-blue-600">First Link</a>
-            <a className="mr-5 hover:text-blue-600">Second Link</a>
-            <a className="mr-5 hover:text-blue-600">Third Link</a>
-            <a className="mr-5 hover:text-blue-600">Fourth Link</a>
-          </nav>
-          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
+          <div name="logo" className="flex ">
+           <img src={Logo} alt="FoodOpp Logo"
+           />
+          </div>
+          <h1 className="px-3 m-2 text-white font-bold">
+            Hello, {userName}
+            
+          </h1>
+
         </div>
       </header>
     )
   }
 
+  //create new request
