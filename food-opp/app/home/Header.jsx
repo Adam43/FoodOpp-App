b55f13ef7f94dc00405bc1cc8f61c9eeeb02bebb
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2023-05-19 10:50:11 -0400

    added header

---
+++ food-opp/app/home/Header.jsx
@@ -0,0 +1,4 @@
+// `app/page.tsx` is the UI for the `/` URL
+export default function Header() {
+    return <h1>Hello, Home page!</h1>;
+  }
---
+++ food-opp/app/home/page.js
@@ -0,0 +1,10 @@
+import Header from "./Header";
+
+// `app/page.tsx` is the UI for the `/` URL
+export default function Page() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+  }
