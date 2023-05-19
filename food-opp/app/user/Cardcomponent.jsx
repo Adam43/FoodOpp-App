b3cba8b8c9fbc14e428f62e0f177c65e02517743
Joselyn <cuzco.joselyn@gmail.com>
2023-05-19 12:08:27 -0400

    ..

---
+++ food-opp/app/user/Cardcomponent.jsx
@@ -0,0 +1,14 @@
+import {} from "react";
+
+
+export default function Cardcomponent() {
+    return(
+      <div className="flex justify-center items-center h-screen">
+        <div className="bg-[#6997f4] p-4 sm:p-8">
+
+        </div>
+
+      </div>
+    )
+
+  }
