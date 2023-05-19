73907b15d76913e0631ba1c7f59701cec006ee08
Dan Kim <dkim729@bloomberg.net>
2023-05-19 16:20:48 -0400

    adding skeleton file for new request

---
+++ food-opp/app/user/new-request/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const NewRequest = ({}) => {
+  return (
+    <>
+      <Link href="/user">BACK</Link>
+      NEW REQ
+    </>
+  );
+};
+
+export default NewRequest;
