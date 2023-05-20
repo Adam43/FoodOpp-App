b256e7baefa527e5840def47360df695f7300fd2
awhitford10 <awhitford10@gmail.com>
2023-05-20 10:04:32 -0400

    added state navigation in vendor page

--- food-opp/app/vendor/vendor-landing-page.js
+++ food-opp/app/vendor/vendor-landing-page.js
@@ -2,21 +2,69 @@
 import Image from 'next/image';
 import { useState } from 'react';
 
+
 export const VendorLandingPage = () => {
+  const [page, setPage] = useState("start")
+  console.log("-----------", page)
+
+  const clickStart = () => {
+    console.log("-----onclick clicked")
+    setPage("ack");
+  }
+
+  const clickAck = () => {
+    console.log("-----onclick clicked")
+    setPage("travel");
+  }
   return (
     <div>
-      <MapComponent />
+      { page==="start" ?
+          <VendorStart setPage={clickStart}/>
+        :
+        page==="ack" ?
+          <VendorAcknowlege setPage={clickAck}/>
+        :
+        page==="travel" ?
+          <VendorTravel/>
+        :
+          <VendorStart setPage={clickStart}/>
+      }
     </div>
   );
 };
 
-export const MapComponent = () => {
-  const [mapClicked, setMapClicked] = useState(false);
 
-  const mapClickedHandler = () => {
-    setMapClicked(!mapClicked);
-  };
 
+export const VendorStart = (props) => {
+  return (
+<section class="text-gray-600 body-font relative">
+            <div class="absolute inset-0 bg-gray-300">
+                <iframe
+                    width="100%"
+                    height="100%"
+                    frameborder="0"
+                    marginheight="0"
+                    marginwidth="0"
+                    title="map"
+                    scrolling="no"
+                    src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=astoria+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
+                    style={{filter: "grayscale(0.2) contrast(1.2) opacity(0.8);"}}>
+                    
+                    </iframe>
+            </div>
+            <div class="container px-5 pt-56 pb-5 mx-auto flex">
+                <div class="flex-row bg-white rounded-lg p-8 flex flex-col w-full mt-10 md:mt-0 relative z-10 shadow-md">
+                   
+                
+                <button onClick={props.setPage} class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Start</button>
+                <p class="text-xs text-gray-500 mt-2">Clicking Acknowledge will start travel directions and send a time estimate to customers</p>
+                </div>
+            </div>
+        </section>
+  );
+};
+
+export const VendorAcknowlege = (props) => {
   return (
     <section class="text-gray-600 body-font relative">
       <div class="absolute inset-0 bg-gray-300">
@@ -44,20 +92,20 @@ export const MapComponent = () => {
           </div>
           <div class="relative">
             <div class="flex flex-row ">
-              <label htmlFor="email" class="text-gray-600 mr-1">
+              <label for="email" class="text-gray-600 mr-1">
                 Food Type:
               </label>
-              <label htmlFor="email" class="text-gray-600 sm:ml-5">
+              <label for="email" class="text-gray-600 sm:ml-5">
                 Ice Cream
               </label>
             </div>
           </div>
           <div class="flex flex-col md:flex-row relative mb-2 md:justify-between ">
-            <label htmlFor="message" class="text-gray-600">
+            <label for="message" class="text-gray-600">
               Count: 34
             </label>
             <label
-              htmlFor="message"
+              for="message"
               class=" sm:text-left md:text-right lg:text-right text-gray-600"
             >
               Expiry Time: 2 Hrs
