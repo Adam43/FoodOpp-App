fe0632a48dad6394d7359cf16d5bce2ce233c4b7
Tim Tran <49887921+tacascer@users.noreply.github.com>
2023-05-20 15:36:43 -0400

    Make home page a column

--- food-opp/app/page.js
+++ food-opp/app/page.js
@@ -6,19 +6,21 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
+    // q: What does this styling do?
+    // a: It makes the main element a flexbox with a minimum height of the screen
     <main className="flex min-h-screen w-full flex-col items-center justify-between p-24">
-      <div className="flex gap-4 w-full">
+      <div className="flex-col gap-4 w-full">
         <Link
           href="/user"
           className="flex justify-center items-center w-full h-96"
         >
-          <Button className="w-full h-60 text-5xl">User page</Button>
+          <Button className="w-full h-60 text-4xl">User page</Button>
         </Link>
         <Link
           href="/vendor"
           className="flex justify-center items-center w-full h-96"
         >
-          <Button className="w-full h-60 text-5xl">Vendor page</Button>
+          <Button className="w-full h-60 text-4xl">Vendor page</Button>
         </Link>
       </div>
     </main>
