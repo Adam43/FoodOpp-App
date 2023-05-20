adca75ed7ec56095a6c4fb707b80395aaabe9349
awhitford10 <awhitford10@gmail.com>
2023-05-20 12:12:57 -0400

    adjusted nav bar for vendor

--- food-opp/app/vendor/Header.js
+++ food-opp/app/vendor/Header.js
@@ -4,12 +4,8 @@ import Image from 'next/image'
 import Caret from 'next/image'; //Having issues with the imag imports
 
 export default function Header() {
-  // const result = await
-
-  const userName = 'Bob';
-
   return (
-    <header className="text-gray-600 flex flex-wrap self-center w-full bg-gray-800 p-0!">
+    <header className="text-gray-600 flex justify-between w-full bg-gray-800 p-0!">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row ">
         <a className="flex title-font font-medium items-center text-gray-100 mb-4 md:mb-0"></a>
         <div name="logo" className="flex self-center">
@@ -22,14 +18,8 @@ export default function Header() {
         </div>
 
         {/*Menu*/}
-        <nav className=" flex flex-wrap items-center text-base justify-center text-gray-100 font-bold">
-          <h1 className="px-3 m-2 text-white items-center font-bold">Hello, {userName}!</h1>
-          <ul className="hidden md:flex nav-pink">
-
-            <a className="hover:text-blue-500 justify-right">
-              <li>Home</li>
-            </a>
-          </ul>
+        <nav className="flex flex-wrap items-center text-base justify-between text-gray-100 font-bold">
+          <h1 className="px-3 m-2 text-white items-center font-bold">Hello, Paletero Inc.</h1>
         </nav>
         <div></div>
       </div>
