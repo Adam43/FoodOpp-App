f72912eb1959cdaf41e82743b2738f42e370e294
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2023-05-20 12:53:06 -0400

    new request page styling

--- food-opp/app/user/new-request/page.js
+++ food-opp/app/user/new-request/page.js
@@ -9,7 +9,7 @@ import toast, { Toaster } from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
 import { ArrowLeftIcon } from '@heroicons/react/24/solid';
 
-const NewRequest = ({}) => {
+const NewRequest = ({ }) => {
   const [name, setName] = useState('');
   const [crowdEstimate, setCrowdEstimate] = useState(0);
   const [foodType, setFoodType] = useState('');
@@ -61,59 +61,62 @@ const NewRequest = ({}) => {
   return (
     <div className="flex flex-col p-4 w-full items-center">
       <Card color="transparent" shadow={false}>
-        <div className="flex">
+        <div className="flex items-center">
           <Link href="/user" className="w-6 h-6 mr-auto">
             <ArrowLeftIcon />
           </Link>
-          <Typography className="mx-auto" variant="h4" color="blue-gray">
+          <Typography className="mr-auto" variant="h4" color="blue-gray">
             Create New Request
           </Typography>
         </div>
-        <Typography color="gray" className="mt-1 font-normal">
-          Enter your event details
+        <Typography color="gray" className="mt-4 font-bold">
+          Enter your event details:
         </Typography>
         <form
-          className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
+          className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96 text-white"
           onSubmit={handleSend}
         >
           <div className="mb-4 flex flex-col gap-6">
-            <label htmlFor="name">Name</label>
+            <label htmlFor="name" className='font-bold'>Event Name</label>
             <input
               id="name"
-              className="border-2 border-black"
+              className="border-2 border-black rounded-lg h-10 pl-2"
               type="text"
               name="name"
+              placeholder='xxx'
               value={name}
               onChange={(event) => setName(event.target.value)}
             />
-            <label htmlFor="location">Location</label>
+            <label htmlFor="location" className='font-bold'>Location</label>
             <input
               id="location"
-              className="border-2 border-black"
+              className="border-2 border-black rounded-lg h-10 pl-2"
               type="text"
               name="location"
+              placeholder='Google URL:'
               value={location}
               onChange={(event) => setLocation(event.target.value)}
             />
-            <label htmlFor="crowd-estimate">Crowd Estimate</label>
+            <label htmlFor="crowd-estimate" className='font-bold'>Crowd Estimate</label>
             <input
               id="crowd-estimate"
-              className="border-2 border-black"
+              className="border-2 border-black rounded-lg h-10 w-20 text-center"
               type="number"
               name="crowd-estimate"
               value={crowdEstimate}
               onChange={(event) => setCrowdEstimate(event.target.value)}
             />
-            <label htmlFor="food-type">Food type</label>
+            <label htmlFor="food-type" className='font-bold'>Food type</label>
             <input
               id="food-type"
-              className="border-2 border-black"
+              className="border-2 border-black pl-2"
               type="text"
               name="food-type"
+              placeholder='Thai'
               value={foodType}
               onChange={(event) => setFoodType(event.target.value)}
             />
-            <label htmlFor="event-duration">Event duration</label>
+            <label htmlFor="event-duration" className='font-bold'>Event duration</label>
             <select
               id="event-duration"
               name="event-duration"
@@ -127,18 +130,25 @@ const NewRequest = ({}) => {
               <option value="90">90 minutes</option>
               <option value="120">120 minutes</option>
             </select>
-            <label htmlFor="notes">Notes</label>
+
+            <label htmlFor="priority" class="relative inline-flex items-center mb-4 cursor-pointer">
+              <input type="checkbox" value="" class="sr-only peer" />
+              <div className="w-11 h-6 bg-red-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-orange-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-orange-600 peer-checked:bg-blue-600"></div>
+              <span className="ml-3 text-lg font-bold text-gray-900 dark:text-blue-500">Priority 🏃💨</span>
+            </label>
+            <label htmlFor="notes" className='self-center font-bold'>Notes</label>
             <textarea
               id="notes"
-              className="border-2 border-black"
+              className="border-2 border-black pl-2"
               name="notes"
+              placeholder="Dairy free...etc"
               value={notes}
               onChange={(event) => setNotes(event.target.value)}
             />
             <input
               type="submit"
               value="REQUEST"
-              className="border bg-blue-500 text-white rounded-lg py-2"
+              className="border bg-blue-500 text-white rounded-lg py-2 font-bold"
             />{' '}
           </div>
           <Typography color="gray" className="mt-4 text-center font-normal">
@@ -147,6 +157,7 @@ const NewRequest = ({}) => {
               href="/user"
               className="font-medium text-blue-500 transition-colors hover:text-blue-700"
             >
+              <br />
               Edit one of your existing requests
             </Link>
           </Typography>
