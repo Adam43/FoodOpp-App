0ff7eb43c777c0e7d7413a5def8c356afd1a5018
Dan Kim <dkim729@bloomberg.net>
2023-05-19 23:24:33 -0400

    commenting out errors in Header.js

--- food-opp/app/user/Header.js
+++ food-opp/app/user/Header.js
@@ -1,31 +1,26 @@
-import { } from "react";
-import Link from "next/link";
-import Logo from "../images/logo.png" //Having issues with the imag imports
-import Caret from "next/image" //Having issues with the imag imports
-
-
+import {} from 'react';
+import Link from 'next/link';
+// import Logo from "../images/logo.png" //Having issues with the imag imports
+import Caret from 'next/image'; //Having issues with the imag imports
 
 export default function Header() {
+  // const result = await
 
-  // const result = await 
-
-  const userName = "Bob"
+  const userName = 'Bob';
 
   return (
     <header className="text-gray-600 flex flex-wrap self-center w-full bg-gray-800 p-0!">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row ">
-        <a className="flex title-font font-medium items-center text-gray-100 mb-4 md:mb-0">
-        </a>
+        <a className="flex title-font font-medium items-center text-gray-100 mb-4 md:mb-0"></a>
         <div name="logo" className="flex self-center">
-          <img src={Logo} alt="FoodOpp Logo"
-          />
+          {/* <img src={Logo} alt="FoodOpp Logo" /> */}
         </div>
-        <div name="profile-pic" className="h-20 w-20 border border-black rounded-full"></div>
-        <img src={"../images/caret.png"} />
-        <h1 className="px-3 m-2 text-white font-bold">
-          Hello, {userName}!
-
-        </h1>
+        <div
+          name="profile-pic"
+          className="h-20 w-20 border border-black rounded-full"
+        ></div>
+        {/* <img src={"../images/caret.png"} /> */}
+        <h1 className="px-3 m-2 text-white font-bold">Hello, {userName}!</h1>
         {/*Menu*/}
         <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center text-gray-100 font-bold">
           <ul className="hidden md:flex nav-pink">
@@ -43,13 +38,10 @@ export default function Header() {
             </a>
           </ul>
         </nav>
-        <div>
-
-        </div>
-
+        <div></div>
       </div>
     </header>
-  )
+  );
 }
 
-  //create new request
+//create new request
